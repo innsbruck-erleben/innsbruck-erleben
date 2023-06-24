07e412f5829b4316c8ec25ef8650d95b2357953a
juba1508 <jule.bauer96@web.de>
2023-06-25 00:24:35 +0200

    Regen und Temperatur implementiert

--- seen/main.js
+++ seen/main.js
@@ -1,4 +1,4 @@
-/* Wetterstationen Euregio Beispiel */
+/* Badeseen und Wetter rund um Innsbruck */
 
 // Innsbruck
 let ibk = {
@@ -8,31 +8,27 @@ let ibk = {
 
 // Karte initialisieren
 let map = L.map("map", {
-    fullscreenControl: true
+    fullscreenControl: true, minZoom: 11,
 }).setView([ibk.lat, ibk.lng], 11);
 
 // thematische Layer
 let themaLayer = {
-    stations: L.featureGroup(),
+    badeseen: L.featureGroup(),
     temperature: L.featureGroup(),
-    windspeed: L.featureGroup(),
-    snowheight: L.featureGroup(),
 }
 
 // Hintergrundlayer
 let layerControl = L.control.layers({
     "Relief avalanche.report": L.tileLayer(
         "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
-        attribution: `© <a href="https://lawinen.report">CC BY avalanche.report</a>`
+        attribution: `© <a href="https://lawinen.report">CC BY avalanche.report</a>`, minZoom: 11,
     }).addTo(map),
     "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
     "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
     "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
 }, {
-    "Wetterstationen": themaLayer.stations,
-    "Temperatur": themaLayer.temperature.addTo(map),
-    "Windgeschwindigkeit": themaLayer.windspeed.addTo(map),
-    "Schneehöhe": themaLayer.snowheight.addTo(map),
+    "Badeseen": themaLayer.badeseen.addTo(map),
+    "Temperatur": themaLayer.temperature,
 }).addTo(map);
 
 layerControl.expand(); //Layer immer offen, muss nicht mehr mit einem Klick geöffnet werden
@@ -49,41 +45,6 @@ function getColor(value, ramp){
         }
     }
 }
-
-
-
-function writeStationLayer(jsondata) {
-// Wetterstationen bearbeiten
-L.geoJSON(jsondata, {
-    pointToLayer: function(feature, latlng) {
-        return L.marker(latlng, {
-            icon: L.icon({
-                iconUrl: `icons/wifi.png`,
-                iconAnchor: [16, 37],
-                popupAnchor: [0, -37],
-            })
-        });
-    },
-
-        onEachFeature: function (feature, layer) {
-            let prop = feature.properties; //Variable damit kürzer; * steht als Platzhalter für Bildunterschrift, Link für Infos, nur 1 Tab für Links
-            let höhenmeter = feature.geometry.coordinates;
-            let pointInTime = new Date(prop.date);
-            console.log(pointInTime);
-            layer.bindPopup(`      
-            <h4>${prop.name} ${höhenmeter[2]} m ü NN</h4>
-            <ul>
-            <li>Lufttemperatur (°C): ${prop.LT||"keine Angabe"}</li>
-            <li>Relative Luftfeuchte (%): ${prop.RH||"keine Angabe"}</li>
-            <li>Windgeschwindigkeit (km/h): ${prop.WG ? (prop.WG*3.6).toFixed(1) : "keine Angabe"}</li>
-            <li>Schneehöhe (cm): ${prop.HS||"keine Angabe"}</li>
-            <li>Regen (mm/m²): ${prop.R||"keine Angabe"}</li>
-            </ul>
-            <span>${pointInTime.toLocaleString()}</span> 
-            `);
-        }
-    }).addTo(themaLayer.stations); //alle Wetterstationen anzeigen als Marker
-    }
 
 function writeTemperatureLayer(jsondata){
     L.geoJSON(jsondata,{
@@ -104,55 +65,13 @@ function writeTemperatureLayer(jsondata){
     }).addTo(themaLayer.temperature);
 }
 
-function writeWindspeedLayer(jsondata){
-    L.geoJSON(jsondata,{
-        filter: function(feature){
-            if (feature.properties.WG >= 0 && feature.properties.WG < 100) {
-                return true;
-            }
-        },
-        pointToLayer: function(feature, latlng) {
-            let color = getColor(feature.properties.WG, COLORS.windspeed);
-            return L.marker(latlng, {
-                icon: L.divIcon({
-                    className: "aws-div-icon",
-                    html: `<span style="background-color:${color}">${(feature.properties.WG).toFixed(1)}</span>`
-                })
-            });
-        },
-    }).addTo(themaLayer.windspeed);
+async function loadTemperature (url) {
+    let response = await fetch(url); //Anfrage, Antwort kommt zurück
+    let jsondata = await response.json(); //json Daten aus Response entnehmen
+    writeTemperatureLayer(jsondata);
 }
 
-function writeSnowheightLayer(jsondata){
-    L.geoJSON(jsondata,{
-        filter: function(feature){
-            if (feature.properties.HS >= 0 && feature.properties.HS < 1000) {
-                return true;
-            }
-        },
-        pointToLayer: function(feature, latlng) {
-            let color = getColor(feature.properties.HS, COLORS.snowheight);
-            return L.marker(latlng, {
-                icon: L.divIcon({
-                    className: "aws-div-icon",
-                    html: `<span style="background-color:${color}">${(feature.properties.HS).toFixed(1)}</span>`
-                })
-            });
-        },
-    }).addTo(themaLayer.snowheight);
-}
-
-
-
-    async function loadStations (url) {
-        let response = await fetch(url); //Anfrage, Antwort kommt zurück
-        let jsondata = await response.json(); //json Daten aus Response entnehmen
-        writeStationLayer(jsondata);
-        writeTemperatureLayer(jsondata);
-        writeWindspeedLayer(jsondata);
-        writeSnowheightLayer(jsondata);
-    }
-loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
+loadTemperature("https://static.avalanche.report/weather_stations/stations.geojson");
 
 L.control.rainviewer({ 
     position: 'bottomleft',
@@ -164,3 +83,36 @@ L.control.rainviewer({
     animationInterval: 500,
     opacity: 0.5
 }).addTo(map);
+
+function writeStationLayer(jsondata) {
+    // Wetterstationen bearbeiten
+    L.geoJSON(jsondata, {
+        pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `icons/wifi.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37],
+                })
+            });
+        },
+    
+            onEachFeature: function (feature, layer) {
+                let prop = feature.properties; //Variable damit kürzer; * steht als Platzhalter für Bildunterschrift, Link für Infos, nur 1 Tab für Links
+                let höhenmeter = feature.geometry.coordinates;
+                let pointInTime = new Date(prop.date);
+                console.log(pointInTime);
+                layer.bindPopup(`      
+                <h4>${prop.name} ${höhenmeter[2]} m ü NN</h4>
+                <ul>
+                <li>Lufttemperatur (°C): ${prop.LT||"keine Angabe"}</li>
+                <li>Relative Luftfeuchte (%): ${prop.RH||"keine Angabe"}</li>
+                <li>Windgeschwindigkeit (km/h): ${prop.WG ? (prop.WG*3.6).toFixed(1) : "keine Angabe"}</li>
+                <li>Schneehöhe (cm): ${prop.HS||"keine Angabe"}</li>
+                <li>Regen (mm/m²): ${prop.R||"keine Angabe"}</li>
+                </ul>
+                <span>${pointInTime.toLocaleString()}</span> 
+                `);
+            }
+        }).addTo(themaLayer.stations); //alle Wetterstationen anzeigen als Marker
+        }
