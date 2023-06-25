a79214b3a71de8f5618795f2fba88ca10fc90c1c
juba1508 <jule.bauer96@web.de>
2023-06-25 23:06:06 +0200

    ausgeklammerte Sachen entfernt

--- seen/main.js
+++ seen/main.js
@@ -14,7 +14,6 @@ let map = L.map("map", {
 // thematische Layer
 let themaLayer = {
     badeseen: L.featureGroup(),
-    //temperature: L.featureGroup(),
 }
 
 // Hintergrundlayer
@@ -24,7 +23,6 @@ let layerControl = L.control.layers({
     "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto", {minZoom: 11}),
 }, {
     "Badeseen": themaLayer.badeseen.addTo(map),
-    //"Temperatur": themaLayer.temperature,
 }).addTo(map);
 
 layerControl.expand(); //Layer immer offen, muss nicht mehr mit einem Klick geöffnet werden
@@ -46,33 +44,6 @@ function getColor(value, ramp){
     }
 }
 
-/*function writeTemperatureLayer(jsondata){
-    L.geoJSON(jsondata,{
-        filter: function(feature){
-            if (feature.properties.LT > -50 && feature.properties.LT < 50) {
-                return true;
-            }
-        },
-        pointToLayer: function(feature, latlng) {
-            let color = getColor(feature.properties.LT, COLORS.temperature);
-            return L.marker(latlng, {
-                icon: L.divIcon({
-                    className: "aws-div-icon",
-                    html: `<span style="background-color:${color}">${feature.properties.LT.toFixed(1)}</span>`
-                })
-            });
-        },
-    }).addTo(themaLayer.temperature);
-}
-
-async function loadTemperature (url) {
-    let response = await fetch(url); //Anfrage, Antwort kommt zurück
-    let jsondata = await response.json(); //json Daten aus Response entnehmen
-    writeTemperatureLayer(jsondata);
-}
-
-loadTemperature("https://static.avalanche.report/weather_stations/stations.geojson");
-*/
 L.control.rainviewer({ 
     position: 'bottomleft',
     nextButtonText: '>',
@@ -110,41 +81,4 @@ async function showBadeseen (url){
     }).addTo(themaLayer.badeseen);
 }
 showBadeseen ("see.geoJson");
-
-
-/*function showBadeseen(jsondata) {
-    // Badeseen bearbeiten
-    L.geoJSON(jsondata, {
-        pointToLayer: function(feature, latlng) {
-            return L.marker(latlng, {
-                icon: L.icon({
-                    iconUrl: `icon\swim.png`,
-                    iconAnchor: [16, 37],
-                    popupAnchor: [0, -37],
-                })
-            });
-        },
-    
-            onEachFeature: function (feature, layer) {
-                let prop = feature.properties; //Variable damit kürzer; * steht als Platzhalter für Bildunterschrift, Link für Infos, nur 1 Tab für Links
-                layer.bindPopup(`      
-                <h4>${prop.BADEGEWÄSSERNAME}</h4>
-                <ul>
-                <li>Lufttemperatur (°C): ${prop.LT||"keine Angabe"}</li>
-                <li>Wassertemperatur (°C): ${prop.W||"keine Angabe"}</li>
-                <li>Wasserqualität: ${prop.A||"keine Angabe"}</li>
-                </ul>
-                <span>${pointInTime.toLocaleString()}</span>
-                `);
-            }
-        }).addTo(themaLayer.badeseen); //alle Badeseen anzeigen als Marker
-        }
-
-        async function loadBadeseen (url) {
-            let response = await fetch(url); //Anfrage, Antwort kommt zurück
-            let jsondata = await response.json(); //json Daten aus Response entnehmen
-            writeBadeseenLayer(jsondata);
-        }
-        showBadeseen ("badeseen.json");*/
-
         
